19b4922b04896a72671ff2a256cd7c8eab7ee251
Lars Kvihaugen <lars@kvihaugen.no>
2026-07-22 00:36:13 +0200

    [development]: continue briefly

--- src/Builder.ts
+++ src/Builder.ts
@@ -1,8 +1,22 @@
 import type { IncomingRequestContentType } from "./types";
+import type { Middleware } from "./Middleware";
+import type { IncomingRequest } from "./IncomingRequest";
 
-export class Builder {
+export class Builder<IR extends IncomingRequest> {
 
     private _contentType: IncomingRequestContentType = null!;
+    private _middlewares: Middleware<any, any>[] = [];
+
+
+    public mw<
+        InIR extends IncomingRequest,
+        Out extends IncomingRequest | unknown // Response
+    >(middleware: Middleware<InIR, Out>) {
+        this._middlewares.push(middleware);
+
+        return this as Builder<Out>;
+    }
+
 
     constructor(__contentType: IncomingRequestContentType) {
         this._contentType = __contentType;
